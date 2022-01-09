29d67e0d45aa687df0bc7672b671ea7dfcaeb2e4
DorAmiel <doradir@gmail.com>
2022-01-09 17:06:29 +0200

    step-2-done

--- index.js
+++ index.js
@@ -1,38 +1,116 @@
 $(() => {
+    // First entrance load(Coins Cards)
 
-    //First entrance load (Coins Cards)
+    let coinCard = "";
 
-    let coinCard = ''
+
     $.ajax({
         url: "https://api.coingecko.com/api/v3/coins/list",
         success: (list) => {
-            displayCard(list)
+            displayCard(list);
         },
-        error: (list) => {}
     });
-
 
     const displayCard = (list) => {
         for (let i = 0; i < 100; i++) {
-            console.table(list[i])
+            // console.table(list[i])
             coinCard += `
             <div class="col">
-                <div id="${i} coin-card" class="card border-light mb-3" style="max-width: 15rem;">
+                <div id="card-container" class="card border-light mb-3 coin-card" style="max-width: 15rem; ">
                     <div class="card-header">${list[i].symbol}</div>
                     <div class="card-body">
                         <h5 class="card-title">${list[i].name}</h5>
-                        <div class="form-check form-switch">
-                            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+                        <div class="custom-control custom-switch">
+                        <input type="checkbox" class="custom-control-input" id="switchID${list[i].id}">
+                        <label class="custom-control-label" for="switchID${list[i].id}"></label>
                         </div>
                         <br>
-                        <button type="button" class="btn btn-outline-primary">More Info</button>
+                        <button id="${list[i].id}" type="button" class="btn btn-outline-primary more-info-button">More Info</button>
+                        <div id="data-place-holder"></div>
+                        <div class="spinner-container" id="spinner${list[i].id}">
+                        <div class="spinner-border text-dark" role="status">
+                        <span class="sr-only">Loading...</span>
+                      </div>
+                        </div>
+                    </div>
                     </div>
                 </div>
-            </div>`
+            `;
+
         }
-        $("#main-container").append(coinCard)
-    }
+        $("#main-container").append(coinCard);
+        $(".spinner-container").hide()
+        clickEventFunction();
+    };
 
     //More Info functions
+    const clickEventFunction = () => {
+        $(".more-info-button").click(function() {
+            $(this).next().collapse("toggle");
+            if ($(this).next().children().length <= 0) {
+                let cardId = $(this).attr("id");
+                $(`#spinner${cardId}`).show()
+                $.ajax({
+                    url: `https://api.coingecko.com/api/v3/coins/${cardId}`,
+                    success: function(data) {
+                        displayCoinInformation(data);
+                    },
+                });
+                const displayCoinInformation = (data) => {
+                    let usdPrice = data.market_data.current_price.usd
+                    let euroPrice = data.market_data.current_price.eur
+                    let ilsPrice = data.market_data.current_price.ils
+                    let coinThumbnail = data.image.thumb
+                    let coinSymbol = data.symbol
+                    let moreInfo = `
+                    <div id="more-info-place-holder">
+                    <br>
+                    <span>${usdPrice} &#36;</span>
+                    <br>
+                    <span>${euroPrice} &#8364;</span>
+                    <br>
+                    <span>${ilsPrice} &#8362;</span>
+                    <br>
+                    <img src="${coinThumbnail}" alt="${coinSymbol} img">
+                    </div>`;
+                    $(this).next().append(moreInfo);
+                    $(`#spinner${cardId}`).hide()
+                    localStorage.setItem("usdPrice", usdPrice)
+                    localStorage.setItem("euroPrice", euroPrice)
+                    localStorage.setItem("ilsPrice", ilsPrice)
+                    localStorage.setItem("coinThumbnail", coinThumbnail)
+                    localStorage.setItem("coinSymbol", coinSymbol)
+                    console.log(localStorage)
 
-})
+
+                };
+            } else {
+                return;
+            }
+        });
+    };
+
+    //Navigate Buttons function
+
+    $("#about-button").click(function() {
+        $("#main-container").hide();
+        $("#live-reports-container").hide();
+        $("#about-container").show();
+    });
+    $("#coins-button").click(function() {
+        $("#live-reports-container").hide();
+        $("#about-container").hide();
+        $("#main-container").show();
+    });
+    $("#live-reports-button").click(function() {
+        $("#main-container").hide();
+        $("#about-container").hide();
+        $("#live-reports-container").show();
+    });
+
+    //$(".more-info-place-holder").each(function() {
+    //let infoId = $(this).attr("id")
+    //$(`#${infoId}`).slideToggle(2000);
+    //console.log(infoId)
+    //})
+});
